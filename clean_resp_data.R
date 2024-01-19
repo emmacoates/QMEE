3cf6491dffd5078fe8fb98518fa501ab8bb4cb8d
Emma Coates <coatee4@mcmaster.ca>
2024-01-19 16:14:44 -0500

    Added file to save cleaned data as .rds file Added file to read cleaned data from .rds file and do things with it

---
+++ clean_resp_data.R
@@ -0,0 +1,31 @@
+# Week 2 assignment 
+# Cleans RESP-NET data 
+# Saves clean version of file as .RDS file
+
+library(tidyverse)
+library(readr)
+library(dplyr)
+
+respTable <- read_csv("Rates_of_Laboratory-Confirmed_RSV__COVID-19__and_Flu_Hospitalizations_from_the_RESP-NET_Surveillance_Systems_20240115.csv")
+
+problems(respTable)
+summary(respTable)
+
+
+print(ggplot(respTable, aes(x=`Weekly Rate`))
+      + geom_histogram()
+)
+
+print(ggplot(respTable, aes(x=`Cumulative Rate`))
+      + geom_histogram()
+)
+
+
+respTableClean <- ( respTable 
+                    %>% filter(!is.na(`Weekly Rate`),
+                               !is.na(`Cumulative Rate`),
+                               !is.na(Sex),
+                               !is.na(`Race/Ethnicity`))
+                    ) 
+
+saveRDS(respTableClean, "respTableClean.rds")
--- resp_data_analysis.R
+++ resp_data_analysis.R
@@ -1,9 +1,12 @@
-# Week 1 assignment 
+# Week 2 assignment 
 # Calculating mean rate of infection for each flu/RSV/COVID for each year 
+# loads .RDS file of cleaned data
 
 library(tidyverse)
+library(readr)
+library(dplyr)
 
-resp_dd <- read_csv("Rates_of_Laboratory-Confirmed_RSV__COVID-19__and_Flu_Hospitalizations_from_the_RESP-NET_Surveillance_Systems_20240115.csv")
+respTableClean <- readRDS("respTableClean.rds")
 
 # Extracts data combined from all genders, races/ethnicities, age groups, and 
 # geographical locations given a specified year and disease monitoring network 
@@ -16,45 +19,45 @@ resp_dd <- read_csv("Rates_of_Laboratory-Confirmed_RSV__COVID-19__and_Flu_Hospit
 # OUTPUT
 # a data frame of combined data for specified year and disease monitoring network
 get_overall_yearly_dd <- function( disease_net, year ) {
-
-    ( resp_dd 
-    %>% filter( Surveillance.Network == disease_net, 
-                MMWR.Year == year,
-                Age.group == 'Overall', 
+  
+  ( respTableClean 
+    %>% filter( `Surveillance Network` == disease_net, 
+                `MMWR Year` == year,
+                `Age group` == 'Overall', 
                 Sex == 'Overall', 
-                Race.Ethnicity == 'Overall', 
+                `Race/Ethnicity` == 'Overall', 
                 Site == 'Overall' ) )
   
 }
 
 # extracting combined data from flu for each year into separate data frames 
 # & calculating mean overall rate for each year for flu 
-mean_rate_flu2018 <- mean(get_overall_yearly_dd('FluSurv-NET', '2018')$Weekly.Rate)
-mean_rate_flu2019 <- mean(get_overall_yearly_dd('FluSurv-NET', '2019')$Weekly.Rate)
-mean_rate_flu2020 <- mean(get_overall_yearly_dd('FluSurv-NET', '2020')$Weekly.Rate)
-mean_rate_flu2021 <- mean(get_overall_yearly_dd('FluSurv-NET', '2021')$Weekly.Rate)
-mean_rate_flu2022 <- mean(get_overall_yearly_dd('FluSurv-NET', '2022')$Weekly.Rate)
-mean_rate_flu2023 <- mean(get_overall_yearly_dd('FluSurv-NET', '2023')$Weekly.Rate)
+mean_rate_flu2018 <- mean(get_overall_yearly_dd('FluSurv-NET', '2018')$'Weekly Rate')
+mean_rate_flu2019 <- mean(get_overall_yearly_dd('FluSurv-NET', '2019')$'Weekly Rate')
+mean_rate_flu2020 <- mean(get_overall_yearly_dd('FluSurv-NET', '2020')$'Weekly Rate')
+mean_rate_flu2021 <- mean(get_overall_yearly_dd('FluSurv-NET', '2021')$'Weekly Rate')
+mean_rate_flu2022 <- mean(get_overall_yearly_dd('FluSurv-NET', '2022')$'Weekly Rate')
+mean_rate_flu2023 <- mean(get_overall_yearly_dd('FluSurv-NET', '2023')$'Weekly Rate')
 mean_rate_flu_yearly <- c(mean_rate_flu2018, mean_rate_flu2019, mean_rate_flu2020, 
                           mean_rate_flu2021, mean_rate_flu2022, mean_rate_flu2023)
 
 # extracting combined data from RSV for each year into separate data frames 
 # & calculating mean overall rate for each year for RSV
-mean_rate_RSV2018 <- mean(get_overall_yearly_dd('RSV-NET', '2018')$Weekly.Rate)
-mean_rate_RSV2019 <- mean(get_overall_yearly_dd('RSV-NET', '2019')$Weekly.Rate)
-mean_rate_RSV2020 <- mean(get_overall_yearly_dd('RSV-NET', '2020')$Weekly.Rate)
-mean_rate_RSV2021 <- mean(get_overall_yearly_dd('RSV-NET', '2021')$Weekly.Rate)
-mean_rate_RSV2022 <- mean(get_overall_yearly_dd('RSV-NET', '2022')$Weekly.Rate)
-mean_rate_RSV2023 <- mean(get_overall_yearly_dd('RSV-NET', '2023')$Weekly.Rate)
+mean_rate_RSV2018 <- mean(get_overall_yearly_dd('RSV-NET', '2018')$'Weekly Rate')
+mean_rate_RSV2019 <- mean(get_overall_yearly_dd('RSV-NET', '2019')$'Weekly Rate')
+mean_rate_RSV2020 <- mean(get_overall_yearly_dd('RSV-NET', '2020')$'Weekly Rate')
+mean_rate_RSV2021 <- mean(get_overall_yearly_dd('RSV-NET', '2021')$'Weekly Rate')
+mean_rate_RSV2022 <- mean(get_overall_yearly_dd('RSV-NET', '2022')$'Weekly Rate')
+mean_rate_RSV2023 <- mean(get_overall_yearly_dd('RSV-NET', '2023')$'Weekly Rate')
 mean_rate_RSV_yearly <- c(mean_rate_RSV2018, mean_rate_RSV2019, mean_rate_RSV2020, 
                           mean_rate_RSV2021, mean_rate_RSV2022, mean_rate_RSV2023)
 
 # extracting combined data from COVID for each year into separate data frames 
 # & calculating mean overall rate for each year for COVID
-mean_rate_COVID2020 <- mean(get_overall_yearly_dd('COVID-NET', '2020')$Weekly.Rate)
-mean_rate_COVID2021 <- mean(get_overall_yearly_dd('COVID-NET', '2021')$Weekly.Rate)
-mean_rate_COVID2022 <- mean(get_overall_yearly_dd('COVID-NET', '2022')$Weekly.Rate)
-mean_rate_COVID2023 <- mean(get_overall_yearly_dd('COVID-NET', '2023')$Weekly.Rate)
+mean_rate_COVID2020 <- mean(get_overall_yearly_dd('COVID-NET', '2020')$'Weekly Rate')
+mean_rate_COVID2021 <- mean(get_overall_yearly_dd('COVID-NET', '2021')$'Weekly Rate')
+mean_rate_COVID2022 <- mean(get_overall_yearly_dd('COVID-NET', '2022')$'Weekly Rate')
+mean_rate_COVID2023 <- mean(get_overall_yearly_dd('COVID-NET', '2023')$'Weekly Rate')
 mean_rate_COVID_yearly <- c(NA, NA, mean_rate_COVID2020, mean_rate_COVID2021, 
                             mean_rate_COVID2022, mean_rate_COVID2023)
 
